c3d2d28198744cac1f048bde8298c80b170d2d05
dvforest <dvforest@hotmail.com>
2026-08-14 08:01:03 -0400

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
+/*
+The goal of the game is to click all cards one after the other.
+You lose if you click the same card twice.
+Upon clicking a card, all cards reshuffle.
+
+Global variable:
+POKEMON_DECK: 
+  {
+    id: 'charizard'
+    name: 'Charizard'
+
+  }
+
+APP
+  > Score Section
+    States: score
+            bestScore
+  > Card Section
+    States: cards
+            SelectedIds
+    Functions: getCard
+  > Card Container
+    States: 
+  > 
+
+
+
+
+*/
+
+
 function App() {
   return (
     <div>
