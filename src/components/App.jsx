5aa5f223c339daabc6084fa7935d8d370b9e6b42
dvforest <dvforest@hotmail.com>
2026-08-24 21:23:06 -0400

    Add loading animation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,69 +1,9 @@
-/*
-The goal of the game is to click all cards one after the other.
-You lose if you click the same card twice.
-Upon clicking a card, all cards reshuffle.
-
-APP
-  States: score (number)
-          bestScore (number)
-          gameState (playing, hasWon, hasLost)
-      useEffect: use localStorage to save bestScore
-      (dependency: bestScore)
-      if loading: return loading screen
-      if hasWon: display win screen, then on clicking play again, display Card Section
-      if hasLost: display Card Section
-
-  > Score Section
-      props: score, bestScore
-  > Card Section
-      props: score, gameState, preloadedDeck, onLoaded
-      States: cards (array of objects with pokemon data)
-              SelectedIds (array of ids)
-      useEffect:
-              async function to call preload deck and assign to cards
-              call onLoaded()
-              (no dependency: runs once)
-      fetchPokemonDeck: return an object with data (id, name, sprite) to assign to cards
-      shuffle(cards):
-        takes an array of cards and return them reshuffled
-      deal(cards): deal cards on the screen in the order they appear in the array
-      handleWin():
-        update bestScore if needed
-        show win screen
-        on clicking play again,
-        reset selectedIds 
-        setCards(shuffle(cards))
-      handleLose():
-        update bestScore  if needed
-        reset selectedIds 
-        setCards(shuffle(cards))
-      handleClick:
-              props: newId 
-                Update SelectedIds with newId
-                if nb of Ids match number of cards > handleWin()
-                if newId exists in SelectedIds > handleLose()
-      return: map all cards
-  > Card
-      Props: pokemon, delay
-      Components:
-              Card Container
-              Pokemon Name
-              Pokemon Sprite 
-              Card Frame
-      style: handle animation using delay
-      return: one card
-  > 
-
-
-
-
-*/
-
 import { useEffect, useState } from 'react';
 import { fetchPokemonDeck } from '../utils/fetchPokemonDeck';
 import { preloadImages } from '../utils/preloadImages';
 import CardSection from './CardSection.jsx';
 import '../styles/app.css';
+import { LoaderCircle } from 'lucide-react';
 
 function App() {
   const [score, setScore] = useState(0);
@@ -123,19 +63,20 @@ function App() {
         <h1>Pokemon Memory Game</h1>
       </header>
 
+      <div className="score-section">
+        <h2>Score: {score}</h2>
+        <h2>Best: {bestScore}</h2>
+      </div>
+
       {gameState === 'isLoading' && (
         <div className="loading-container">
-          <div className="loading-animation" />
-          <div className="loading-text">Loading...</div>
+          <LoaderCircle className="loading-animation" />
+          <h2 className="loading-text">Loading...</h2>
         </div>
       )}
 
       {gameState === 'isPlaying' && (
         <main>
-          <div className="score-section">
-            <h2>Score: {score}</h2>
-            <h2>Best: {bestScore}</h2>
-          </div>
           <CardSection
             deck={deck}
             onShuffle={handleShuffle}
